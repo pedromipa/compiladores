d4fcff2f23954ea0273d8251930ad954e2e422a4
Pedro Mian Parra <55238366+pedromipa@users.noreply.github.com>
2022-09-05 13:39:51 -0300

    simple buffered lexical analyzer

---
+++ AnalisadorLexico/src_BufferSimples/GyhLex.java
@@ -0,0 +1,15 @@
+package teste_analisador;
+
+public class GyhLex {
+
+	public static void main(String[] args) {
+		GyhLexico lex = new GyhLexico(args[0]);
+		Token t = null;
+		
+		while((t = lex.proximoToken()).nome != TipoToken.Fim) {
+			System.out.println(t);
+		}
+
+	}
+
+}
