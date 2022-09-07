84d61b37d43c3e1da62b92645b95a0c40b64d2a1
Pedro Mian Parra <pedro.mipa@hotmail.com>
2022-09-06 22:12:00 -0300

    folder organization

--- AnalisadorLexico/src/lexico/TipoToken.java
+++
@@ -1,9 +0,0 @@
-package lexico;
-
-public enum TipoToken {
-PCDec, PCProg, PCInt,PCReal,PCLer,PCImprimir,PCSe,PCSenao,PCEntao,
-PCEnqto,PCIni,PCFim,OPAritMult,OpAritDiv,OpAritSoma,OpAritSub,
-OpRelMenor,OpRelMenorIgual,OpRelMaior,OpRelMaiorIgual,OpRelIgual,
-OpRelDif,OpBoolE,OpBoolOu,Delim,Atrib,AbrePar,FechaPar,Var,NumInt,
-NumReal,Cadeia,Fim
-}
